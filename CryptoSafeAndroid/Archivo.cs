3458daadbec290ad47cb8738bda57dac9cc116c5
Alexis Aguirre <aaaguirrer@hotmail.com>
2019-10-14 10:44:59 -0500

    Agregada estructura de fila en listView

---
+++ CryptoSafeAndroid/Archivo.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+
+namespace CryptoSafeAndroid
+{
+    class Archivo
+    {
+        public string Nombre { get; set; }
+        public double Tamano { get; set; }
+        public string Extension { get; set; }
+
+        public string ArchivoSinExtension()
+        {
+            return Path.GetFileNameWithoutExtension(Nombre);
+        }
+    }
+}
